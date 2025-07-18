c66e378b6d8c8de53eea8a25fadea06eebaa16ca
shivalaxmirapan <shivalaxmi.weblabs@gmail.com>
2025-07-18 17:25:41 +0530

    Add /clear-cache route to fix Railway 500 error

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Support\Facades\Artisan;
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
@@ -11,3 +12,11 @@ Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show')
 
 Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
 
+
+Route::get('/clear-cache', function () {
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('route:clear');
+    Artisan::call('view:clear');
+    return 'Cache cleared!';
+});
